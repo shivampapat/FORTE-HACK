e8259ca94e828ff74fac99c559254371570cab6e
Shivam Papat <shivampapat@gmail.com>
2020-07-08 18:16:46 +0530

    Login Singin Updated

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FirebaseAuthService} from '../services/firebase-auth.service';
+
+//Angular Form builder
+
+import { FormBuilder, Validators, FormGroup } from '../../../node_modules/@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import{Observable} from 'rxjs';
+
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -7,9 +17,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  //data members
+  nForm:FormGroup;
+  post:any;
+  username:string;
+  password:string;
+loginInfo :Observable<any[]>;
 
-  ngOnInit(): void {
+
+  constructor(private fb : FormBuilder ,public fauth:FirebaseAuthService) { }
+
+  //Functions
+  public Copy(post)
+  {
+    this.username = post.name;
+    this.password = post.descriptor;
+    this.display();
+    this.fauth.SignUp(this.username, this.password);
   }
 
+  public  display()
+    {
+      console.log(this.username);
+      console.log(this.password);
+    }
+
+  ngOnInit(): void {
+
+    this.nForm = this.fb.group({
+      'name'       : [null, Validators.required],
+      'descriptor': [null,Validators.compose([Validators.required,Validators.maxLength(20) ]) ],
+      'validate'    : ''
+
+    });
+  }
 }
